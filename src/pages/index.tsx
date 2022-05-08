86effa016a00785c58754b0d51bf30e3686adb68
Weydson Marinho <weydsonmarinho@gmail.com>
2022-05-08 13:25:45 -0300

    Home: added the Load More button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,7 +63,17 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            marginTop="40px"
+            type="button"
+            onClick={() => fetchNextPage()}
+            loadingText="Carregando..."
+            isLoading={isFetchingNextPage}
+          >
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );

0b895181956ad414949e60b54fd5829c86ef7def
Weydson Marinho <weydsonmarinho@gmail.com>
2022-05-07 21:32:02 -0300

    CardList: Modal and Rendering Cards

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,26 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [imageURL, setImageURL] = useState('');
 
-  // TODO SELECTED IMAGE URL STATE
-
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string) {
+    setImageURL(url);
+    onOpen();
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing="40px" maxWidth="960px">
+        {cards.map(card => {
+          const { id, url } = card;
 
-      {/* TODO MODALVIEWIMAGE */}
+          return (
+            <Card key={id} viewImage={() => handleViewImage(url)} data={card} />
+          );
+        })}
+      </SimpleGrid>
+      <ModalViewImage isOpen={isOpen} onClose={onClose} imgUrl={imageURL} />
     </>
   );
 }

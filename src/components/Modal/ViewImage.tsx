5095274352fae82db46dc28cf94f1d06e8aa164b
Weydson Marinho <weydsonmarinho@gmail.com>
2022-05-07 21:26:48 -0300

    Modal/ViewImage styled and displaying information passed to it

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -20,4 +20,39 @@ export function ModalViewImage({
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal onClose={onClose} isOpen={isOpen}>
+      <ModalOverlay opacity="0.5" />
+      <ModalContent
+        bgColor="pGray.900"
+        display="flex"
+        alignItems="center"
+        width="auto"
+        height="auto"
+        maxHeight="600px"
+        maxWidth="900px"
+      >
+        <ModalBody padding="0" width="100%" height="100%">
+          <Image
+            src={imgUrl}
+            width="100%"
+            height="100%"
+            maxHeight="600px"
+            maxWidth="900px"
+          />
+        </ModalBody>
+        <ModalFooter
+          backgroundColor="pGray.800"
+          width="100%"
+          justifyContent="flex-start"
+          fontSize="14px"
+          color="pGray.50"
+        >
+          <Link href={imgUrl} target="_blank">
+            <a>Abrir original</a>
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
